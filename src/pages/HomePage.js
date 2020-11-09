19171227c8335a6323657e53527aaa988a9bcb7e
Dax Wann <daxwann@gmail.com>
2020-11-08 18:40:01 -0800

    set up react and create HomePage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    <div>
+      <h1>My Home Page</h1>
+      <p>Some content</p>
+    </div>;
+  }
+}
+
+export default HomePage;
